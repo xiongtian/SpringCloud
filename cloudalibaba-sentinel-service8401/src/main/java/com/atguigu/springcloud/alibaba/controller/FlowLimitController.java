4219de46419628d8c578921488037b67b655bae9
熊天 <764578422@qq.com>
2020-11-10 14:40:04 +0800

    sentinel环境搭建

---
+++ cloudalibaba-sentinel-service8401/src/main/java/com/atguigu/springcloud/alibaba/controller/FlowLimitController.java
@@ -0,0 +1,23 @@
+package com.atguigu.springcloud.alibaba.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author xiongtian
+ * @version 1.0
+ * @date 2020/11/10 13:51
+ */
+@RestController
+public class FlowLimitController {
+
+    @GetMapping("/testA")
+    public String testA(){
+        return "------testA";
+    }
+
+    @GetMapping("/testB")
+    public String testB(){
+        return "------testB";
+    }
+}
