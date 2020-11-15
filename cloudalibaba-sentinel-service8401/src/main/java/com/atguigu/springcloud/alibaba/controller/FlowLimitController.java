51c2da6e413a1883c761917aaa437dfd653d6ea2
xiongtian <764578422@qq.com>
2020-11-15 20:04:21 +0800

    sentinel降级

--- cloudalibaba-sentinel-service8401/src/main/java/com/atguigu/springcloud/alibaba/controller/FlowLimitController.java
+++ cloudalibaba-sentinel-service8401/src/main/java/com/atguigu/springcloud/alibaba/controller/FlowLimitController.java
@@ -1,7 +1,10 @@
 package com.atguigu.springcloud.alibaba.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author xiongtian
@@ -9,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @date 2020/11/10 13:51
  */
 @RestController
+@Slf4j
 public class FlowLimitController {
 
     @GetMapping("/testA")
@@ -19,5 +23,26 @@ public class FlowLimitController {
     @GetMapping("/testB")
     public String testB(){
         return "------testB";
+    }
+
+    @GetMapping("/testD")
+    public String testD(){
+        // 暂停几秒钟线程
+       // 测试RT try { TimeUnit.SECONDS.sleep(1); } catch (InterruptedException e) { e.printStackTrace(); }
+        // log.info("testD 测试RT");
+
+        // 测试异常比例
+        int a=10/0;
+        log.info("testD 测试异常比例 ");
+
+        return "-------testD";
+    }
+
+    @GetMapping("/testE")
+    public String testE(){
+        log.info("testE 测试异常数！");
+
+        int age=10/0;
+        return "-------testE 测试异常数！";
     }
 }
